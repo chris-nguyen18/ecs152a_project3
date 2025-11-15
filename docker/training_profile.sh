7dc4f26da0408e445ae0a42886717221e02a1225
Shaoor Munir <shaoormunir@gmail.com>
2025-11-15 04:30:00 -0800

    Update training_profile.sh

--- docker/training_profile.sh
+++ docker/training_profile.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Public Test Profile - Predictable patterns for debugging
 bandwidth=50000  # 50 Mbps baseline
 delay=50         # 50ms baseline RTT
 pl=0.1          # 0.1% baseline loss
 
-# Initial setup - Apply traffic shaping to loopback interface (lo) for localhost traffic
 tc qdisc add dev lo root handle 1: htb default 1
 tc class add dev lo parent 1: classid 1:1 htb rate ${bandwidth}kbit ceil ${bandwidth}kbit
 tc qdisc add dev lo parent 1:1 handle 10: netem delay ${delay}ms loss ${pl}%
